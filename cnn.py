2b7db91044ae51d5cc05b36dbb54e6c4bd245b78
pseudo-sm <saswathcommand@gmail.com>
2019-01-05 17:36:31 +0530

    Update cnn.py
    Added comments

--- cnn.py
+++ cnn.py
@@ -3,13 +3,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,Dropout,Activation, Flatten, Conv2D,MaxPooling2D
 
 import pickle
+# read vectors from pickles
+
 X = pickle.load(open("X.pickle","rb"))
 y = pickle.load(open("y.pickle","rb"))
 
+# Normalize features vector
 X = X/255.0
 
+# Constructing a sequential model ....
 model = Sequential()
-
 
 model.add(Conv2D(64,(3,3),input_shape=X.shape[1:]))
 model.add(Activation("relu"))
@@ -28,6 +31,7 @@ model.add(Activation('sigmoid'))
 model.compile(loss="binary_crossentropy",
               optimizer="adam",
               metrics = ["accuracy"])
+# if you've got a good system try with higher number epochs that might tune your accuracy higher
 model.fit(X,y,batch_size=32,epochs=3,validation_split=0.1)
 
 
