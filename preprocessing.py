108325a64c7f37f0e67ecf3e206476bb39cc796c
pseudo-sm <saswathcommand@gmail.com>
2019-01-05 17:32:46 +0530

    Update preprocessing.py
    Added Comments !

--- preprocessing.py
+++ preprocessing.py
@@ -4,11 +4,10 @@ import os
 
 import cv2
 
-#Mention the path where your data directory is stored. 
-
-
-
+# path where your data directory is stored. Subdirectories in the data_directory
+# must have the name of the classes of the image inside them.
 DATADIR = '/path/to/data_directory'
+# Names of all classes in a list
 CATEGORIES = ['Class A','Class B','Class C']
 
 IMG_SIZE = 200
@@ -19,8 +18,8 @@ def create_training_data():
         path = os.path.join(DATADIR,category)
         class_num = CATEGORIES.index(category)
         for img in os.listdir(path):
-            print(img)
             try:
+                #pick each image from each directory and convert it into a 200 X 200 image in gray scale.
                 img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
                 new_array = cv2.resize(img_array, (IMG_SIZE,IMG_SIZE))
                 training_data.append([new_array,class_num])
@@ -28,16 +27,16 @@ def create_training_data():
                 pass
             
 create_training_data()
-print(len(training_data))
 
 X = []
 y = []
 for features,label in training_data:
     X.append(features)
     y.append(label)
-
+# Generate Features and Labels vector
 X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
 
+# Pickle both vectors so you dont have to do it every time.
 import pickle
 pickle_out = open("X.pickle",'wb')
 pickle.dump(X,pickle_out)
@@ -49,6 +48,4 @@ pickle_out.close()
 
 pickle_in = open("X.pickle","rb")
 x = pickle.load(pickle_in)
-
-print(x)
 
